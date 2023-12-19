174307f20e638d30e5268b537d17ca5091a17e45
Patryk241194 <potrykus.patrykk@gmail.com>
2023-12-19 19:42:02 +0100

    First commit

---
+++ src/main/java/com/kodilla/onlinecurrencyexchangebackend/OnlineCurrencyExchangeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.onlinecurrencyexchangebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCurrencyExchangeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCurrencyExchangeBackendApplication.class, args);
+	}
+
+}
